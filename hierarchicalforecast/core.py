d4f10ad1b22b8a5994251babd263dadee21a1860
David Luo <davidluogolf@gmail.com>
2023-04-16 23:09:45 -0400

    print debugging

--- hierarchicalforecast/core.py
+++ hierarchicalforecast/core.py
@@ -134,10 +134,13 @@ class HierarchicalReconciliation:
         # Declare output names
         drop_cols = ['ds', 'y'] if 'y' in Y_hat_df.columns else ['ds']
         model_names = Y_hat_df.drop(columns=drop_cols, axis=1).columns.to_list()
+        print(len(Y_hat_df[model_names].select_dtypes(include='number').columns))
         pi_model_names = [name for name in model_names if ('-lo' in name or '-hi' in name)]
         model_names = [name for name in model_names if name not in pi_model_names]
         
         # Ensure numeric columns
+        print(len(Y_hat_df[model_names].select_dtypes(include='number').columns))
+        print(len(model_names))
         if not len(Y_hat_df[model_names].select_dtypes(include='number').columns) == len(model_names):
             raise Exception('Check `Y_hat_df`s columns contain numeric types')
 
