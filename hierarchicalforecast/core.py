fedcf1b5cba0d9c24a0d194590e8f932dfd02b2a
Olivier Sprangers <45119856+elephaint@users.noreply.github.com>
2025-04-23 15:05:40 +0200

    [FIX] Add error message when y_hat contains missing timestamps (#367)

--- hierarchicalforecast/core.py
+++ hierarchicalforecast/core.py
@@ -169,6 +169,12 @@ class HierarchicalReconciliation:
                     f"Check `S_df` columns, {reprlib.repr(id_col)} must be in `S_df` columns."
                 )
 
+        # Check if Y_hat_df has the right shape
+        if len(Y_hat_nw.group_by(id_col).agg(nw.len()).unique(subset="len")) != 1:
+            raise ValueError(
+                "Check `Y_hat_df`, there are missing timestamps. All series should have the same number of predictions."
+            )
+
         # -------------------------------- Match Y_hat/Y/S index order --------------------------------#
         # TODO: This is now a bit slow as we always sort.
         S_nw = S_nw.with_columns(**{f"{id_col}_id": np.arange(len(S_nw))})
