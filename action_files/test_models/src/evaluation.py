31ba7ad935387c04e9f96f2c44015745bbac2165
Olivier Sprangers <osprangers@gmail.com>
2025-06-20 18:16:59 +0200

    update_ci

--- action_files/test_models/src/evaluation.py
+++ action_files/test_models/src/evaluation.py
@@ -1,12 +1,14 @@
+import fire
 import pickle
 import numpy as np
 import pandas as pd
 
 import hierarchicalforecast.evaluation as hfe
-from utilsforecast.losses import rmse, mase
+from utilsforecast.losses import rmse, mase,scaled_crps
 from functools import partial
+from typing import Optional
 
-def eval():
+def eval(level: Optional[list[int]] = None) -> pd.DataFrame:
     execution_times = pd.read_csv('data/execution_times.csv')
     models = [f"{x[0]} ({x[1]:.2f} secs)" for x in execution_times.values]
 
@@ -17,28 +19,19 @@ def eval():
     with open('data/tags.pickle', 'rb') as handle:
         tags = pickle.load(handle)
 
-    eval_tags = {}
-    eval_tags['Total'] = tags['Country']
-    eval_tags['State'] = tags['Country/State']
-    eval_tags['Regions'] = tags['Country/State/Region']
-    eval_tags['Bottom'] = tags['Country/State/Region/Purpose']
-    eval_tags['All'] = np.concatenate(list(tags.values()))
-
     mase_p = partial(mase, seasonality=4)
     evaluation = hfe.evaluate(
             df=Y_rec_df.merge(Y_test_df, on=['unique_id', 'ds'], how="left"),
-            metrics = [rmse, mase_p],
-            tags=eval_tags, 
+            metrics = [rmse, mase_p, scaled_crps],
+            level=level,
+            tags=tags, 
             train_df=Y_train_df
     )
     numeric_cols = evaluation.select_dtypes(include="number").columns
     evaluation[numeric_cols] = evaluation[numeric_cols].map('{:.3}'.format).astype(np.float64)
     evaluation.columns = ['level', 'metric', 'Base'] + models
-    return evaluation
-
-
+    print(evaluation.T)
+    evaluation.to_csv('./data/evaluation.csv')
 
 if __name__ == '__main__':
-    evaluation = eval()
-    evaluation.to_csv('./data/evaluation.csv')
-    print(evaluation.T)
+    fire.Fire(eval)
